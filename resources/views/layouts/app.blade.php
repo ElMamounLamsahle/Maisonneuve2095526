c8a43461d75ed4f250a3d4936bd3bb4ac6d40e5d
ElMamounLamsahle <mlamsahle@gmail.com>
2022-05-24 10:31:04 -0400

    Correction route accueil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li class="nav-item"><a class="nav-link" href="/"><i class="bi bi-house-door-fill"></i> @lang('lang.text_home')</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{ route('etudiants') }}"><i class="bi bi-house-door-fill"></i> @lang('lang.text_home')</a></li>
                             @guest
                                 <li class="nav-item"><a class="nav-link" href="{{ route('login.form') }}"><i class="bi bi-box-arrow-in-right"></i> @lang('lang.text_connection')</a></li>
                                 <li class="nav-item"><a class="nav-link" href="{{ route('registration.form') }}"><i class="bi bi-person-plus-fill"></i> @lang('lang.text_registration')</a></li>
